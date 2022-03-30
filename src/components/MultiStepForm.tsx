aec7bee5dfbe5d5e5b5f03f77819c304f386f047
dimon08888 <dimon08888@mail.ru>
2022-03-30 14:18:00 +0300

    Добавлена валидация шагов и submit алерт

--- src/components/MultiStepForm.tsx
+++ src/components/MultiStepForm.tsx
@@ -2,29 +2,39 @@ import { useState } from 'react';
 import { validateLogin, validateEmail, validatePassword } from '../utils/validators';
 
 export default function MultiStepForm() {
-  // const [login, setLogin] = useState('');
-  // const [loginError, setLoginError] = useState('');
+  const [login, setLogin] = useState('');
+  const [loginError, setLoginError] = useState('');
 
-  // const [password, setPassword] = useState('');
-  // const [passwordError, setPasswordError] = useState('');
+  const [email, setEmail] = useState('');
+  const [emailError, setEmailError] = useState('');
 
-  // const [email, setEmail] = useState('');
-  // const [emailError, setEmailError] = useState('');
-
-  // function handleSubmit(e: React.FormEvent<HTMLFormElement>) {
-  //   e.preventDefault();
-  //   setLoginError(validateLogin(login));
-  //   setPasswordError(validatePassword(password));
-  //   setEmailError(validateEmail(email));
-  // }
+  const [password, setPassword] = useState('');
+  const [passwordError, setPasswordError] = useState('');
 
   const [step, setStep] = useState<'login' | 'email' | 'password'>('login');
 
+  const handleSubmit: React.FormEventHandler<HTMLFormElement> = e => {
+    e.preventDefault();
+    const passwordErrorMsg = validatePassword(password);
+    setPasswordError(passwordErrorMsg);
+    if (!passwordErrorMsg) {
+      window.alert(JSON.stringify({ login, email, password }, null, 2));
+    }
+  };
+
   function next() {
     if (step === 'login') {
-      setStep('email');
+      const loginErrorMsg = validateLogin(login);
+      setLoginError(loginErrorMsg);
+      if (!loginErrorMsg) {
+        setStep('email');
+      }
     } else if (step === 'email') {
-      setStep('password');
+      const emailErrorMsg = validateEmail(email);
+      setEmailError(emailErrorMsg);
+      if (!emailErrorMsg) {
+        setStep('password');
+      }
     }
   }
 
@@ -38,70 +48,91 @@ export default function MultiStepForm() {
 
   return (
     <div className="flex justify-center border-2 w-52 h-52">
-      <div className="flex flex-col">
-        {step === 'login' ? (
-          <Login onNext={next} />
-        ) : step === 'email' ? (
-          <Email onNext={next} onBack={back} />
-        ) : (
-          <Password onBack={back} />
-        )}
-      </div>
-      {/* <form className="flex flex-col" onSubmit={e => handleSubmit(e)}>
+      <form className="flex flex-col" onSubmit={handleSubmit}>
         <h1>Multi step form</h1>
-        <input
-          type="text"
-          placeholder="login"
-          className="border-2 border-solid border-black rounded block"
-          onChange={e => setLogin(e.target.value)}
-          value={login}
-          required
-        />
-
-        <button
-          type="submit"
-          className="bg-blue-500 px-2 py-1 rounded text-white hover:bg-blue-600"
-        >
-          Next
-        </button>
-      </form> */}
+        <div className="flex flex-col">
+          {step === 'login' ? (
+            <>
+              <h1>Login</h1>
+              <input
+                type="text"
+                placeholder="login"
+                className="border-2 border-solid border-black rounded block"
+                onChange={e => setLogin(e.target.value)}
+                value={login}
+                required
+              />
+              {loginError ? <div className="text-red-500">{loginError}</div> : null}
+              <button
+                type="button"
+                className="bg-blue-500 rounded text-white px-2 py-2 disabled:bg-blue-300"
+                onClick={next}
+                disabled={login.length === 0}
+              >
+                Next
+              </button>
+            </>
+          ) : step === 'email' ? (
+            <>
+              <h1>Email</h1>
+              <input
+                type="email"
+                onChange={e => setEmail(e.target.value)}
+                value={email}
+                required
+              />
+              {emailError ? <div className="text-red-500">{emailError}</div> : null}
+              <button
+                type="button"
+                className="bg-blue-500 rounded text-white px-2 py-2"
+                onClick={back}
+              >
+                Back
+              </button>
+              <button
+                type="button"
+                className="bg-blue-500 rounded text-white px-2 py-2 disabled:bg-blue-300"
+                onClick={next}
+                disabled={email.length === 0}
+              >
+                Next
+              </button>
+            </>
+          ) : (
+            <>
+              <h1>Password</h1>
+              <input
+                type="password"
+                onChange={e => setPassword(e.target.value)}
+                value={password}
+                required
+              />
+              {passwordError ? <div className="text-red-500">{passwordError}</div> : null}
+              <button
+                type="button"
+                className="bg-blue-500 rounded text-white px-2 py-2"
+                onClick={back}
+              >
+                Back
+              </button>
+              <button
+                type="submit"
+                className="bg-blue-500 rounded text-white px-2 py-2 disabled:bg-blue-300"
+                disabled={password.length === 0}
+              >
+                Submit
+              </button>
+            </>
+          )}
+        </div>
+      </form>
     </div>
   );
 }
 
-function Login({ onNext }: { onNext: () => void }) {
-  return (
-    <div>
-      <h1>Login</h1>
-      <button className="bg-blue-500 rounded text-white px-2 py-2" onClick={onNext}>
-        Next
-      </button>
-    </div>
-  );
-}
-
-function Email({ onNext, onBack }: { onNext: () => void; onBack: () => void }) {
-  return (
-    <div>
-      <h1>Email</h1>
-      <button className="bg-blue-500 rounded text-white px-2 py-2" onClick={onBack}>
-        Back
-      </button>
-      <button className="bg-blue-500 rounded text-white px-2 py-2" onClick={onNext}>
-        Next
-      </button>
-    </div>
-  );
-}
-
-function Password({ onBack }: { onBack: () => void }) {
-  return (
-    <div>
-      <h1>Password</h1>
-      <button className="bg-blue-500 rounded text-white px-2 py-2" onClick={onBack}>
-        Back
-      </button>
-      <button className="bg-blue-500 rounded text-white px-2 py-2">Submit</button>
-    </div>
-  );
-}
+// TODOS:
+// 1. password repeat
+// 2. persist state in local storage (works after refresh)
+// 3. auto focus input if it is empty on next step.
+// 4. add circles that indicate steps, active circle is bright, inactive dull
+// 5. click on a circle, goes to a step
