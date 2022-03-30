89038e1e2cb1512f1edfa36d0ae4810391151999
dimon08888 <dimon08888@mail.ru>
2022-03-30 13:04:51 +0300

    Добавлен переход по шагам MultiStepForm

--- src/components/MultiStepForm.tsx
+++ src/components/MultiStepForm.tsx
@@ -1,26 +1,53 @@
 import { useState } from 'react';
-import { validateEmail } from '../utils/validators';
+import { validateLogin, validateEmail, validatePassword } from '../utils/validators';
 
 export default function MultiStepForm() {
-  const [login, setLogin] = useState('');
-  const [loginError, setLoginError] = useState(false);
+  // const [login, setLogin] = useState('');
+  // const [loginError, setLoginError] = useState('');
 
-  const [password, setPassword] = useState('');
-  const [passwordError, setPasswordError] = useState(false);
+  // const [password, setPassword] = useState('');
+  // const [passwordError, setPasswordError] = useState('');
 
-  const [email, setEmail] = useState('');
-  const [emailError, setEmailError] = useState('');
+  // const [email, setEmail] = useState('');
+  // const [emailError, setEmailError] = useState('');
 
-  function handleSubmit(e: React.FormEvent<HTMLFormElement>) {
-    e.preventDefault();
-    setLoginError(login.length >= 3);
-    setPasswordError(password.length >= 8);
-    setEmailError(validateEmail(email));
+  // function handleSubmit(e: React.FormEvent<HTMLFormElement>) {
+  //   e.preventDefault();
+  //   setLoginError(validateLogin(login));
+  //   setPasswordError(validatePassword(password));
+  //   setEmailError(validateEmail(email));
+  // }
+
+  const [step, setStep] = useState<'login' | 'email' | 'password'>('login');
+
+  function next() {
+    if (step === 'login') {
+      setStep('email');
+    } else if (step === 'email') {
+      setStep('password');
+    }
+  }
+
+  function back() {
+    if (step === 'email') {
+      setStep('login');
+    } else if (step === 'password') {
+      setStep('email');
+    }
   }
 
   return (
-    <div className="flex justify-center">
-      <form className="flex flex-col" onSubmit={e => handleSubmit(e)}>
+    <div className="flex justify-center border-2 w-52 h-52">
+      <div className="flex flex-col">
+        {step === 'login' ? (
+          <Login onNext={next} />
+        ) : step === 'email' ? (
+          <Email onNext={next} onBack={back} />
+        ) : (
+          <Password onBack={back} />
+        )}
+      </div>
+      {/* <form className="flex flex-col" onSubmit={e => handleSubmit(e)}>
         <h1>Multi step form</h1>
         <input
           type="text"
@@ -30,14 +57,51 @@ export default function MultiStepForm() {
           value={login}
           required
         />
-        {}
+
         <button
           type="submit"
           className="bg-blue-500 px-2 py-1 rounded text-white hover:bg-blue-600"
         >
           Next
         </button>
-      </form>
+      </form> */}
+    </div>
+  );
+}
+
+function Login({ onNext }: { onNext: () => void }) {
+  return (
+    <div>
+      <h1>Login</h1>
+      <button className="bg-blue-500 rounded text-white px-2 py-2" onClick={onNext}>
+        Next
+      </button>
+    </div>
+  );
+}
+
+function Email({ onNext, onBack }: { onNext: () => void; onBack: () => void }) {
+  return (
+    <div>
+      <h1>Email</h1>
+      <button className="bg-blue-500 rounded text-white px-2 py-2" onClick={onBack}>
+        Back
+      </button>
+      <button className="bg-blue-500 rounded text-white px-2 py-2" onClick={onNext}>
+        Next
+      </button>
+    </div>
+  );
+}
+
+function Password({ onBack }: { onBack: () => void }) {
+  return (
+    <div>
+      <h1>Password</h1>
+      <button className="bg-blue-500 rounded text-white px-2 py-2" onClick={onBack}>
+        Back
+      </button>
+      <button className="bg-blue-500 rounded text-white px-2 py-2">Submit</button>
     </div>
   );
 }
